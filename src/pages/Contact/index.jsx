cce018a245a32f9f353950ee1095c5a414e12b40
unknown <frankmnganga@gmail.com>
2020-09-06 18:43:23 +0300

    Contact Page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import FlashText from '../../components/FlashText';
+import ContactForm from '../../components/ContactForm';
+import { ReactComponent as MailSvg } from '../../assets/mail.svg';
+import './contact.styles.scss';
 
 const Contact = () => {
+	const text = ['feedback', 'suggestions', 'ideas', 'support', 'projects'];
+
 	return (
-		<div className="contact">
-			<h2>Get in touch</h2>
-			<p>
-				For feedback, suggestions, ideas and support, you can message me here
+		<div className="root">
+			<h1 className="heading">Get in touch</h1>
+			<p className="text">
+				For <FlashText text={text} />, you can connect with me from here
 			</p>
+			<Paper elevation={3} className="contact">
+				<Grid container>
+					<Grid item xs="auto" sm="auto" md={6}>
+						<MailSvg className="svg" />
+					</Grid>
+					<Grid item xs={12} sm={12} md={6}>
+						<ContactForm />
+					</Grid>
+				</Grid>
+			</Paper>
 		</div>
 	);
 };
