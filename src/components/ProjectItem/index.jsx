41354c943b8e7f540ce6d79f30d5db0e6a2ba6dd
unknown <frankmnganga@gmail.com>
2020-09-08 17:24:10 +0300

    Mail icon

--- src/components/ProjectItem/index.jsx
+++ src/components/ProjectItem/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useHistory, useLocation } from 'react-router';
+import { useHistory } from 'react-router';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
--- src/pages/ComingSoon/index.jsx
+++ src/pages/ComingSoon/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import Button from '@material-ui/core/Button';
 import { ReactComponent as SubscribeSvg } from '../../assets/subscribe.svg';
+import MailIcon from '../../assets/mail.png';
 import './coming-styles.scss';
 
 const ComingSoon = () => {
@@ -14,12 +16,13 @@ const ComingSoon = () => {
 				</span>
 			</h1>
 			<p className="subscribe-text">
-				I am currently working on this and other articles on my blog as well
+				I am currently working on creating the content for this project and
+				other projects on my blog as well
 			</p>
 
 			<p className="subscribe-text">
-				Subscribe here To get notified by email whenever I publish any of my
-				articles
+				Subscribe here to get notified by email whenever I publish any of my
+				stuff
 			</p>
 			<div className="subscribe-form">
 				<SubscribeSvg className="svg" />
@@ -30,6 +33,13 @@ const ComingSoon = () => {
 						placeholder="Email"
 						type="email"
 						required
+						InputProps={{
+							startAdornment: (
+								<InputAdornment position="start">
+									<img src={MailIcon} alt="mail" className="icon" />
+								</InputAdornment>
+							),
+						}}
 					/>
 					<Button
 						color="primary"
@@ -40,6 +50,13 @@ const ComingSoon = () => {
 						Subscribe
 					</Button>
 				</form>
+				<p className="subscribe-text">
+					You will not receive any spam emails from me{' '}
+					<span role="img" aria-label="smile">
+						😃
+					</span>
+					and you can always unsubscribe anytime
+				</p>
 			</div>
 		</div>
 	);
