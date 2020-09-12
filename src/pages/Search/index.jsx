b74af012e472b8478625b378230efe20ab83e622
unknown <frankmnganga@gmail.com>
2020-09-12 15:23:36 +0300

    handled search with useSearch hook

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,24 +1,39 @@
 import React, { useState } from 'react';
 import { ReactComponent as SearchSvg } from '../../assets/search.svg';
 import SearchField from '../../components/SearchField';
+// import { useSelector, useDispatch } from "react-redux";
+import useSearch from '../../hooks/useSearch';
 
 import './search.styles.scss';
+import SearchLink from '../../components/SearchLinks';
 
 const SearchPage = () => {
 	const [focus, setFocus] = useState();
+	const [results, setResults] = useState([]);
+	const { textSearch } = useSearch();
 
+	const handleSearch = (text) => {
+		const results = textSearch(text);
+		setResults(results);
+	};
+
+	console.log({ results });
 	return (
 		<div className="root">
 			<SearchField
 				className={focus ? 'focus' : ''}
 				onFocus={() => setFocus(true)}
-				// onClickAway={() => setFocus(false)}
+				onSubmit={handleSearch}
 			/>
 
-			<div className="info">
-				<p>Looking for something? Search here</p>
-				<SearchSvg className="search-svg" />
-			</div>
+			{!results.length ? (
+				<div className="info">
+					<p>Looking for something? Search here</p>
+					<SearchSvg className="search-svg" />
+				</div>
+			) : (
+				results.map((item) => <SearchLink item={item} key={item.title} />)
+			)}
 		</div>
 	);
 };
