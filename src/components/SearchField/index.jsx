07a2533d71fadb3638ed3d82d9e9249b8ca7a629
unknown <frankmnganga@gmail.com>
2020-09-12 15:21:56 +0300

    Modified search field to handle submit

--- src/components/SearchField/index.jsx
+++ src/components/SearchField/index.jsx
@@ -1,33 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import { ReactComponent as SearchIcon } from '../../assets/search-icon.svg';
 import './search-field.styles.scss';
 
-const SearchField = ({ className = '', onFocus, onClickAway }) => {
+const SearchField = ({ className = '', onFocus, onSubmit, ...props }) => {
 	let classes = 'search ' + className;
+	const [searchText, setSearchtext] = useState('');
 
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		onSubmit(searchText);
+	};
+
+	useEffect(() => {
+		onSubmit(searchText);
+		//eslint-disable-next-line
+	}, [searchText]);
 	return (
-		<ClickAwayListener onClickAway={onClickAway}>
-			<form>
-				<TextField
-					className={classes}
-					placeholder="Search"
-					type="search"
-					InputProps={{
-						startAdornment: (
-							<InputAdornment position="start">
-								<SearchIcon />
-							</InputAdornment>
-						),
-					}}
-					fullWidth
-					variant="outlined"
-					onFocus={onFocus}
-				/>
-			</form>
-		</ClickAwayListener>
+		<form onSubmit={handleSubmit}>
+			<TextField
+				className={classes}
+				placeholder="Search"
+				type="search"
+				InputProps={{
+					startAdornment: (
+						<InputAdornment position="start">
+							<SearchIcon />
+						</InputAdornment>
+					),
+				}}
+				fullWidth
+				variant="outlined"
+				onFocus={onFocus}
+				value={searchText}
+				onChange={({ target }) => setSearchtext(target.value)}
+			/>
+		</form>
 	);
 };
 
