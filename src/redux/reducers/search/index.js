10e80600ed3094355fa8f973849b0d5c8d7adac2
unknown <frankmnganga@gmail.com>
2020-09-12 15:48:39 +0300

    search-results reducer

---
+++ src/redux/reducers/search/index.js
@@ -0,0 +1,10 @@
+const initialState = [{ term: '', results: [] }];
+
+const searchReducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+export default searchReducer;
