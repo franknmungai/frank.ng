7d252adf8d261bb0c88a3be99d8035da4a66bf9f
unknown <frankmnganga@gmail.com>
2020-09-14 20:10:10 +0300

    user reducer, create and cancel email subscription

--- src/redux/reducers/user/index.js
+++ src/redux/reducers/user/index.js
@@ -1,6 +1,7 @@
 import * as types from '../../actions/action-types';
 const initialState = {
 	email: '',
+	subscribed: false,
 	theme: {
 		light: true,
 	},
@@ -10,6 +11,18 @@ export default (state = initialState, action) => {
 	switch (action.type) {
 		case types.TOGGLE_THEME:
 			return { ...state, theme: { light: !state.theme.light } };
+		case types.CREATE_SUBSCRIPTION:
+			return {
+				...state,
+				email: action.email,
+				subscribed: true,
+			};
+		case types.CANCEL_SUBSCRIPTION:
+			return {
+				...state,
+				email: '',
+				subscribed: false,
+			};
 
 		default:
 			return state;
